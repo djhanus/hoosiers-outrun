5c4666af84100100923148bf38dada7a515932d3
djhanus <david@thisisblueline.com>
2019-07-11 10:34:41 -0400

    sponsors as 1 image mod

--- page-sponsors.php
+++ page-sponsors.php
@@ -14,59 +14,9 @@
       </div>
     </div> <!-- /.intro-section -->
 
-    <div class="sponsors-section-large">
-      <?php 
-        $images_L = get_field('sponsors_large');
-        $size_L = 'medium'; // (thumbnail, medium, large, full or custom size)
-
-        if( $images_L ): ?>
-            <ul>
-                <?php foreach( $images_L as $image_L ): ?>
-                    <li class="large">
-                      <?php echo wp_get_attachment_image( $image_L['ID'], $size ); ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-      <?php endif; ?>
-    </div> <!-- /.sponsors-section -->
-
-
-
-    <div class="sponsors-section-medium">
-      <?php 
-        $images_M = get_field('sponsors_medium');
-        $size_M = 'medium'; // (thumbnail, medium, large, full or custom size)
-
-        if( $images_M ): ?>
-            <ul>
-                <?php foreach( $images_M as $image_M ): ?>
-                    <li class="medium">
-                      <?php echo wp_get_attachment_image( $image_M['ID'], $size ); ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-      <?php endif; ?>
-    </div> <!-- /.sponsors-section -->
-
-
-
-    <div class="sponsors-section-small">
-      <?php 
-        $images_S = get_field('sponsors_small');
-        $size_S = 'medium'; // (thumbnail, medium, large, full or custom size)
-
-        if( $images_S ): ?>
-            <ul>
-                <?php foreach( $images_S as $image_S ): ?>
-                    <li class="small">
-                      <?php echo wp_get_attachment_image( $image_S['ID'], $size ); ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-      <?php endif; ?>
-    </div> <!-- /.sponsors-section -->
-
-
+    <div class="sponsors-logos">
+      <img src="<?php echo get_field('sponsors_logos'); ?>" />
+    </div> <!-- /.sponsors-logos -->
 
     <div class="sponsors-list">
       <?php echo get_field('sponsors_list'); ?>
